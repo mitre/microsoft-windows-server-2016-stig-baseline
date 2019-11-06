9135858b85683bf154a8dbbcad12058747450480
Krishna Kola <kkola@digitalinfuzion.com>
2019-11-06 10:16:00 -0500

    update V-73255 for readability

--- controls/V-73255.rb
+++ controls/V-73255.rb
@@ -104,8 +104,6 @@ control 'V-73255' do
   ]
 
   paths.each do |path|
-    acl_rules = json(command: "(Get-ACL -Path '#{path}').Access | ConvertTo-CSV | ConvertFrom-CSV | ConvertTo-JSON").params
-
     if path == "HKLM:\\\\Security"
       acl_rules = json(command: "[Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('Security', 'Default', 'ReadPermissions').GetAccessControl().access | ConvertTo-CSV | ConvertFrom-CSV | ConvertTo-JSON").params
       describe.one do
@@ -135,191 +133,192 @@ control 'V-73255' do
           end
         end
       end
-    elsif path == "HKLM:\\\\Software"
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "FullControl" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "CREATOR OWNER" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+    else
+      acl_rules = json(command: "(Get-ACL -Path '#{path}').Access | ConvertTo-CSV | ConvertFrom-CSV | ConvertTo-JSON").params
+      if path == "HKLM:\\\\Software"
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "FullControl" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "CREATOR OWNER" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "FullControl" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "FullControl" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "FullControl" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "FullControl" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "ReadKey" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "BUILTIN\\Users" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "ReadKey" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "BUILTIN\\Users" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "ReadKey" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "ReadKey" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-    elsif path == "HKLM:\\\\System"
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "268435456" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "CREATOR OWNER" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "InheritOnly" }
+      elsif path == "HKLM:\\\\System"
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "268435456" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "CREATOR OWNER" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "InheritOnly" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "ReadKey" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "BUILTIN\\Users" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "ReadKey" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "BUILTIN\\Users" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "268435456" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "InheritOnly" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "268435456" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "InheritOnly" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "FullControl" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "None" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "FullControl" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "None" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "268435456" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "InheritOnly" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "268435456" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "InheritOnly" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "FullControl" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "None" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "FullControl" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "None" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "ReadKey" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "None" }
-            its(['PropagationFlags']) { should cmp "None" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "ReadKey" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "None" }
+              its(['PropagationFlags']) { should cmp "None" }
+            end
           end
         end
-      end
-
-      describe.one do
-        acl_rules.each do |acl_rule|
-          describe "The '#{path}' key\'s access rule property:" do
-            subject { acl_rule }
-            its(['RegistryRights']) { should cmp "-2147483648" }
-            its(['AccessControlType']) { should cmp "Allow" }
-            its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
-            its(['IsInherited']) { should cmp "False" }
-            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
-            its(['PropagationFlags']) { should cmp "InheritOnly" }
+  
+        describe.one do
+          acl_rules.each do |acl_rule|
+            describe "The '#{path}' key\'s access rule property:" do
+              subject { acl_rule }
+              its(['RegistryRights']) { should cmp "-2147483648" }
+              its(['AccessControlType']) { should cmp "Allow" }
+              its(['IdentityReference']) { should cmp "APPLICATION PACKAGE AUTHORITY\\ALL APPLICATION PACKAGES" }
+              its(['IsInherited']) { should cmp "False" }
+              its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+              its(['PropagationFlags']) { should cmp "InheritOnly" }
+            end
           end
         end
       end
     end
-
   end
 end  
    
