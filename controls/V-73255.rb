4da692c629ec4eedf037ab6cd5edc5c06f578a82
Krishna Kola <kkola@digitalinfuzion.com>
2019-11-06 10:03:52 -0500

    fixes issue #22

--- controls/V-73255.rb
+++ controls/V-73255.rb
@@ -107,7 +107,34 @@ control 'V-73255' do
     acl_rules = json(command: "(Get-ACL -Path '#{path}').Access | ConvertTo-CSV | ConvertFrom-CSV | ConvertTo-JSON").params
 
     if path == "HKLM:\\\\Security"
-      
+      acl_rules = json(command: "[Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('Security', 'Default', 'ReadPermissions').GetAccessControl().access | ConvertTo-CSV | ConvertFrom-CSV | ConvertTo-JSON").params
+      describe.one do
+        acl_rules.each do |acl_rule|
+          describe "The '#{path}' key\'s access rule property:" do
+            subject { acl_rule }
+            its(['RegistryRights']) { should cmp "FullControl" }
+            its(['AccessControlType']) { should cmp "Allow" }
+            its(['IdentityReference']) { should cmp "NT AUTHORITY\\SYSTEM" }
+            its(['IsInherited']) { should cmp "False" }
+            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+            its(['PropagationFlags']) { should cmp "None" }
+          end
+        end
+      end
+
+      describe.one do
+        acl_rules.each do |acl_rule|
+          describe "The '#{path}' key\'s access rule property:" do
+            subject { acl_rule }
+            its(['RegistryRights']) { should cmp "ReadPermissions, ChangePermissions" }
+            its(['AccessControlType']) { should cmp "Allow" }
+            its(['IdentityReference']) { should cmp "BUILTIN\\Administrators" }
+            its(['IsInherited']) { should cmp "False" }
+            its(['InheritanceFlags']) { should cmp "ContainerInherit" }
+            its(['PropagationFlags']) { should cmp "None" }
+          end
+        end
+      end
     elsif path == "HKLM:\\\\Software"
       describe.one do
         acl_rules.each do |acl_rule|
