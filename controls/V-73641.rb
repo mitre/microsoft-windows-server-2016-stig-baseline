8bdaa6f888c42317afb0584cbf187d8c7417b094
Dami Afolabi <dami.afolabi@icloud.com>
2023-06-07 15:45:26 -0400

    declared and used an input for V-73641
    Signed-off-by: Dami Afolabi <dami.afolabi@icloud.com>

--- controls/V-73641.rb
+++ controls/V-73641.rb
@@ -1,10 +1,10 @@
 control 'V-73641' do
-  title "The maximum age for machine account passwords must be configured to 30
+  title "The maximum age for machine account passwords must be configured to #{input('maximum_password_age_machine')}
   days or less."
   desc "Computer account passwords are changed automatically on a regular
   basis. This setting controls the maximum password age that a machine account
-  may have. This must be set to no more than 30 days, ensuring the machine
-  changes its password monthly."
+  may have. This must be set to no more than #{input('maximum_password_age_machine')} days, ensuring the machine
+  changes its password #{input('maximum_password_age_machine')} days."
   impact 0.5
   tag "gtitle": 'SRG-OS-000480-GPOS-00227'
   tag "gid": 'V-73641'
@@ -14,7 +14,7 @@ control 'V-73641' do
   tag "cci": ['CCI-000366']
   tag "nist": ['CM-6 b', 'Rev_4']
   tag "documentable": false
-  desc "check", "This is the default configuration for this setting (30 days).
+  desc "check", "This is the default configuration for this setting (#{input('maximum_password_age_machine')} days).
 
   If the following registry value does not exist or is not configured as
   specified, this is a finding.
@@ -25,16 +25,16 @@ control 'V-73641' do
   Value Name: MaximumPasswordAge
 
   Value Type: REG_DWORD
-  Value: 0x0000001e (30) (or less, but not 0)"
-  desc "fix", "This is the default configuration for this setting (30 days).
+  Value: 0x0000001e (#{input('maximum_password_age_machine')}) (or less, but not 0)"
+  desc "fix", "This is the default configuration for this setting (#{input('maximum_password_age_machine')} days).
 
   Configure the policy value for Computer Configuration >> Windows Settings >>
   Security Settings >> Local Policies >> Security Options >> Domain member:
-  Maximum machine account password age to 30 or less (excluding 0,
+  Maximum machine account password age to #{input('maximum_password_age_machine')} or less (excluding 0,
   which is unacceptable)."
   describe registry_key('HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Services\\Netlogon\\Parameters') do
     it { should have_property 'MaximumPasswordAge' }
-    its('MaximumPasswordAge') { should be <= 30 }
+    its('MaximumPasswordAge') { should be <= input('maximum_password_age_machine') }
   end
   describe registry_key('HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Services\\Netlogon\\Parameters') do
     it { should have_property 'MaximumPasswordAge' }
